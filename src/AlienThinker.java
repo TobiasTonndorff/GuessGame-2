c3c2e17f7ae0a70c9d137b11466ca1772c3959d5
Tobias Tonndorff <cph-tt160@cphbusiness.dk>
2022-11-01 11:49:32 +0100

    test
    tsar

--- src/AlienThinker.java
+++ src/AlienThinker.java
@@ -4,3 +4,31 @@ public class AlienThinker  {
 
   
 }
+=======
+public class AlienThinker implements IThinker{
+
+    @Override
+    public void yourTurn() {
+        
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void newGame(int min, int max, String opponent, String type) {
+
+    }
+
+    @Override
+    public Answer evaluateGuess(int guess, String opponent) {
+        return null;
+    }
+
+    @Override
+    public boolean endOfGame(int numberOfGuesses, String opponent) {
+        return false;
+    }
+}
