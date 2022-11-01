fb7b3fda6229320473a257bee4681d4b4ae6c0c5
Tobias Tonndorff <cph-tt160@cphbusiness.dk>
2022-11-01 11:46:38 +0100

    Update AlienThinker.java

--- src/AlienThinker.java
+++ src/AlienThinker.java
@@ -1,29 +1,5 @@
 
 
-public class AlienThinker implements IThinker {
+public class AlienThinker {
 
-    @Override
-    public void yourTurn() {
-
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public void newGame(int min, int max, String opponent, String type) {
-
-    }
-
-    @Override
-    public Answer evaluateGuess(int guess, String opponent) {
-        return null;
-    }
-
-    @Override
-    public boolean endOfGame(int numberOfGuesses, String opponent) {
-        return false;
-    }
 }
